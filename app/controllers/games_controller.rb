e947b2fc9cde584f768937a4d759c8dc127628eb
Byron Roark <byronroark@gmail.com>
2015-10-05 01:38:37 -0400

    1-7 tests passed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,24 @@
 class GamesController < ActionController::API
+  before_action :set_game, only: [:show, :check]
+
+  def show
+    render json: @game.as_json
+  end
+
+  def check
+    row = params[:row].to_i
+    col = params[:col].to_i
+    @game.check(row, col)
+    @game.update(board: @game.board)
+    render json: @game.as_json
+  end
+
   def create
     @game = Game.create(difficulty: params[:difficulty], state: "new")
     render json: @game.as_json, status: :created
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 end
